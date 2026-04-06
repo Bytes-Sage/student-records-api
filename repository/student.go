11e4f0fbf75d70de7533c09f9f213bdcfdb482ea
Bytes-Sage <gd1610056@gmail.com>
2026-04-06 13:24:04 +0100

    commit

--- repository/student.go
+++ repository/student.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"database/sql"
 	"errors"
 	"students-api/db"
 	"students-api/models"
@@ -45,5 +46,109 @@ func GetAll() ([]models.Student, error) {
 }
 
 func GetByID(id int) (*models.Student, error) {
-	query := `SELECT id, first_name`
+	query := `SELECT id, first_name, last_name, email, age, course, created_at
+	FROM students
+	WHERE id = ?`
+
+	var s models.Student
+	err := db.DB.QueryRow(query, id).Scan(
+		&s.ID,
+		&s.FirstName,
+		&s.LastName,
+		&s.Age,
+		&s.Course,
+		&s.CreatedAt,
+	)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNotFound
+		}
+		return nil, err
+
+	}
+	return &s, nil
+
+}
+
+func Create(req models.CreateStudentRequest) (*models.Student, error) {
+	query := `INSERT INTO students (first_name, last_name, email, age, course)
+	VALUES (?,?,?,?,?)`
+
+	result, err := db.DB.Exec(query,
+		req.FirstName,
+		req.LastName,
+		req.Email,
+		req.Age,
+		req.Course,
+	)
+
+	if err != nil {
+		if isUniqueConstraintErr(err) {
+			return nil, ErrEmailExists
+		}
+		return nil, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+	return GetByID(int(id))
+
+}
+
+func Update(id int, req models.UpdateStudentRequest) (*models.Student, error) {
+	_, err := GetByID(id)
+	if err != nil {
+		return nil, err
+	}
+	query := `UPDATE students
+			SET first_name = ?, last_name = ?, email = ?, age = ?, course = ?
+			WHERE id = ?`
+
+	_, err = db.DB.Exec(query,
+		req.FirstName,
+		req.LastName,
+		req.Email,
+		req.Age,
+		req.Course,
+		id,
+	)
+
+	if err != nil {
+		if isUniqueConstraintErr(err) {
+			return nil, ErrEmailExists
+		}
+		return nil, err
+	}
+	return GetByID(id)
+}
+
+func Delete(id int) error {
+	_, err := GetByID(id)
+	if err != nil {
+		return err
+	}
+	_, err = db.DB.Exec("DELETE FROM students WHERE id = ?", id)
+	return err
+}
+
+func isUniqueConstraintErr(err error) bool {
+	return err != nil && errors.Is(err, errors.New("UNIQUE constraint failed: students.email")) ||
+		constainsString(err.Error(), "UNIQUE constraint failed")
+}
+
+func constainsString(s, substr string) bool {
+	return len(s) >= len(substr) && (s == substr ||
+		len(s) > 0 && constainsSubstring(s, substr))
+}
+
+func constainsSubstring(s, substr string) bool {
+	for i := 0; i <= len(s)-len(substr); i++ {
+		if s[i:i+len(substr)] == substr {
+			return true
+		}
+	}
+	return false
 }
